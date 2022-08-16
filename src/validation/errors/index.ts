76f7e817984dd58840c792df07f00573772eca55
ivansimplicio <ivansimplicio@outlook.com>
2022-08-16 20:42:35 -0300

    feat: created the MissingParamError

--- src/validation/errors/index.ts
+++ src/validation/errors/index.ts
@@ -2,3 +2,4 @@ export * from './invalid-cpf-error';
 export * from './invalid-date-error';
 export * from './invalid-email-error';
 export * from './invalid-password-error';
+export * from './missing-param-error';
